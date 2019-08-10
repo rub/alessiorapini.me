23477b1601372f7cc60a5fe52598ef6a2a8b04d3
Alessio Rapini <alessio.rapini@yahoo.it>
2019-08-10 16:17:39 +0200

    Create Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,23 @@
 import React from "react"
+import PropTypes from "prop-types"
 
-import styles from "./styles.css"
+import styles from "./Header.module.css"
 
+/**
+ * Header displays information relating to the website
+ */
 const Header = ({ authorName, authorRole }) => (
-  <header className={styles.header}>
+  <header className={styles.Header}>
     <p>{authorName}</p>
     <p>{authorRole}</p>
   </header>
 )
+
+Header.propTypes = {
+  /** Author name */
+  authorName: PropTypes.string.isRequired,
+  /** Authore role */
+  authorRole: PropTypes.string.isRequired,
+}
 
 export default Header
