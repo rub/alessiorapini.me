c4a84a56515cd09747423c878dcdc694bee1191e
Alessio Rapini <alessio.rapini@yahoo.it>
2019-09-21 12:39:33 +0200

    Use requestAnimationFrame to run parallax on main picture

--- src/components/RubParallax/RubParallax.js
+++ src/components/RubParallax/RubParallax.js
@@ -1,13 +1,64 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
 import styles from "./RubParallax.module.css"
 
+const useAnimationFrame = callback => {
+  // Use useRef for mutable variables that we want to persist
+  // without triggering a re-render on their change
+  const requestRef = React.useRef()
+  const previousTimeRef = React.useRef()
+
+  const animate = time => {
+    if (previousTimeRef.current != undefined) {
+      const deltaTime = time - previousTimeRef.current
+      callback(deltaTime)
+    }
+    previousTimeRef.current = time
+    requestRef.current = requestAnimationFrame(animate)
+  }
+
+  React.useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate)
+    return () => cancelAnimationFrame(requestRef.current)
+  }, []) // Make sure the effect runs only once
+}
+
 /**
  * Mouse parallax scene on Rub's pictures
  */
 const RubParallax = () => {
+  // Remember normalized component X position (needed for smoothness)
+  const xRef = useRef(0)
+
+  // Remember normalized mouse X position
+  const targetXRef = useRef(0.5)
+
+  const leftRef = useRef()
+  const rightRef = useRef()
+
+  // Add event listener for mouse movement on ComponentDidMount
+  useEffect(() => {
+    const eventListener = window.addEventListener("mousemove", e => {
+      targetXRef.current = e.clientX / document.body.clientWidth - 0.5
+    })
+    return () => window.removeEventListener("mousemove", eventListener)
+  }, [])
+
+  useAnimationFrame(deltaTime => {
+    const deltaX = targetXRef.current - xRef.current
+    xRef.current = xRef.current + deltaX / 16
+
+    leftRef.current.props.imgStyle.transform = `translate3d(calc(-50% + ${xRef.current *
+      40}px), 0, 0)`
+    rightRef.current.props.imgStyle.transform = `translate3d(calc(-50% - ${xRef.current *
+      40}px), 0, 0)`
+
+    leftRef.current.forceUpdate()
+    rightRef.current.forceUpdate()
+  })
+
   const images = useStaticQuery(graphql`
     query {
       fixedRub: file(relativePath: { eq: "rub-fixed.png" }) {
@@ -42,10 +93,10 @@ const RubParallax = () => {
         fluid={images.fixedRub.childImageSharp.fluid}
         imgStyle={{
           left: "50%",
-          marginLeft: "17px",
-          transform: "translateX(-50%)",
+          transform: "translate3d(-50%, 0, 0)",
           width: "auto",
         }}
+        ref={leftRef}
       />
       <Img
         className={styles.parallaxedRubRight}
@@ -54,10 +105,10 @@ const RubParallax = () => {
         fluid={images.fixedRub.childImageSharp.fluid}
         imgStyle={{
           left: "50%",
-          marginLeft: "-17px",
-          transform: "translateX(-50%)",
+          transform: "translate3d(-50%, 0, 0)",
           width: "auto",
         }}
+        ref={rightRef}
       />
     </div>
   )
