285e1dca002c24886482319ba61813db37706194
Alessio Rapini <alessio.rapini@yahoo.it>
2019-10-23 22:46:09 +0200

    Create Works wrapper componenent

---
+++ src/components/Works/Works.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import styles from "./Works.module.css"
+
+const Works = () => <div className={styles.Works}></div>
+
+export default Works
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,11 +2,13 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import Works from "../components/Works/Works"
 import About from "../components/About/About"
 
 const Portfolio = () => (
   <Layout>
     <SEO title="Portfolio" />
+    <Works />
     <About />
   </Layout>
 )
