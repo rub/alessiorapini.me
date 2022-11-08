e811eda64089bd32e225271ef1c475e450c63dcd
Alessio Rapini <alessio.rapini@yahoo.it>
2022-11-08 20:07:48 +0100

    Temporarely comment strapi plugin reference in Gatsby config

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -36,18 +36,18 @@ module.exports = {
         postCssPlugins: [require(`postcss-preset-env`)({ stage: 0 })],
       },
     },
-    {
-      resolve: "gatsby-source-strapi",
-      options: {
-        apiURL: "http://localhost:1337",
-        contentTypes: [
-          // List of the Content Types you want to be able to request from Gatsby.
-          "work",
-          "about",
-          "user",
-        ],
-        queryLimit: 1000,
-      },
-    },
+    // {
+    //   resolve: "gatsby-source-strapi",
+    //   options: {
+    //     apiURL: "http://localhost:1337",
+    //     contentTypes: [
+    //       // List of the Content Types you want to be able to request from Gatsby.
+    //       "work",
+    //       "about",
+    //       "user",
+    //     ],
+    //     queryLimit: 1000,
+    //   },
+    // },
   ],
 }
