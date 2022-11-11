6d21c21b187515a1b96f81c7a77f28873d5ba918
Alessio Rapini <alessio.rapini@yahoo.it>
2022-11-12 00:33:02 +0100

    Use the new gatsby-plugin-image to process the homepage's pictures

--- frontend/src/components/RubParallax/RubParallax.js
+++ frontend/src/components/RubParallax/RubParallax.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import { GatsbyImage, withArtDirection, getImage } from "gatsby-plugin-image"
 
 import {
   rubParallax,
@@ -11,59 +11,64 @@ import {
   parallaxedRubRight,
 } from "./RubParallax.module.css"
 
-const useAnimationFrame = (callback) => {
-  // Use useRef for mutable variables that we want to persist
-  // without triggering a re-render on their change
-  const requestRef = React.useRef()
-  const previousTimeRef = React.useRef()
-
-  const animate = (time) => {
-    if (previousTimeRef.current != undefined) {
-      const deltaTime = time - previousTimeRef.current
-      callback(deltaTime)
-    }
-    previousTimeRef.current = time
-    requestRef.current = requestAnimationFrame(animate)
-  }
-
-  useEffect(() => {
-    requestRef.current = requestAnimationFrame(animate)
-    return () => cancelAnimationFrame(requestRef.current)
-  }, []) // Make sure the effect runs only once
-}
-
 /**
  * Mouse parallax scene on Rub's pictures
  */
 const RubParallax = () => {
-  // Query Rub images
-  const data = useStaticQuery(graphql`
-    query {
-      mobileImage: file(relativePath: { eq: "rub-small.png" }) {
+  // Query images
+  const images = useStaticQuery(graphql`
+    {
+      largeImage: file(relativePath: { eq: "rub-big.png" }) {
         childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid_withWebp
-          }
+          gatsbyImageData(
+            layout: CONSTRAINED
+            placeholder: BLURRED
+            formats: [AUTO, WEBP, AVIF]
+            quality: 25
+          )
         }
       }
-      desktopImage: file(relativePath: { eq: "rub-big.png" }) {
+      smallImage: file(relativePath: { eq: "rub-small.png" }) {
         childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid_withWebp
-          }
+          gatsbyImageData(
+            layout: CONSTRAINED
+            placeholder: BLURRED
+            formats: [AUTO, WEBP, AVIF]
+            quality: 25
+          )
         }
       }
     }
   `)
 
-  // Define which image to provide based on viewport width
-  const sources = [
-    data.mobileImage.childImageSharp.fluid,
+  // Art direction
+  const imagesWithArtDirection = withArtDirection(getImage(images.largeImage), [
     {
-      ...data.desktopImage.childImageSharp.fluid,
-      media: `(min-width: 600px)`,
+      media: "(max-width: 760px)",
+      image: getImage(images.smallImage),
     },
-  ]
+  ])
+
+  const useAnimationFrame = (callback) => {
+    // Use useRef for mutable variables that we want to persist
+    // without triggering a re-render on their change
+    const requestRef = React.useRef()
+    const previousTimeRef = React.useRef()
+
+    const animate = (time) => {
+      if (previousTimeRef.current != undefined) {
+        const deltaTime = time - previousTimeRef.current
+        callback(deltaTime)
+      }
+      previousTimeRef.current = time
+      requestRef.current = requestAnimationFrame(animate)
+    }
+
+    useEffect(() => {
+      requestRef.current = requestAnimationFrame(animate)
+      return () => cancelAnimationFrame(requestRef.current)
+    }, []) // Make sure the effect runs only once
+  }
 
   // Remember normalized component X position (needed for smoothness)
   const xRef = useRef(0)
@@ -71,7 +76,6 @@ const RubParallax = () => {
   // Remember normalized mouse X position
   const targetXRef = useRef(0.5)
 
-  const fixedRef = useRef()
   const leftRef = useRef()
   const rightRef = useRef()
 
@@ -87,68 +91,56 @@ const RubParallax = () => {
     const deltaX = targetXRef.current - xRef.current
     xRef.current = xRef.current + deltaX / 16
 
-    // The higher the X(px) value the higher the translation
-    fixedRef.current.props.imgStyle.transform = `translateX(0%)`
-    leftRef.current.props.imgStyle.transform = `translate3d(calc(-3% - ${
+    leftRef.current.style.transform = `translate3d(calc(-14px - ${
       xRef.current * 16
     }px), 0, 0)`
-    rightRef.current.props.imgStyle.transform = `translate3d(calc(3% + ${
+    rightRef.current.style.transform = `translate3d(calc(14px + ${
       xRef.current * 16
     }px), 0, 0)`
-
-    fixedRef.current.forceUpdate()
-    leftRef.current.forceUpdate()
-    rightRef.current.forceUpdate()
   })
 
   return (
     <div className={rubParallax}>
       <div className={fixedRubContainer}>
-        <Img
+        <GatsbyImage
+          image={imagesWithArtDirection}
           className={fixedRub}
-          alt="Alessio Rapini's picture"
+          alt="Alessio Rapini"
+          loading="eager"
+          objectFit="contain"
+          objectPosition="right"
           draggable={false}
-          fluid={sources}
-          imgStyle={{
+          style={{
             height: "100%",
-            left: "auto",
-            right: "0",
-            width: "auto",
           }}
-          ref={fixedRef}
-          fadeIn={false}
         />
       </div>
-      <div className={parallaxedRubContainer}>
-        <Img
+      <div className={parallaxedRubContainer} ref={leftRef}>
+        <GatsbyImage
+          image={imagesWithArtDirection}
           className={parallaxedRubLeft}
-          alt="Alessio Rapini's picture with parallax effect"
+          alt="Alessio Rapini animated"
+          loading="eager"
+          objectFit="contain"
+          objectPosition="right"
           draggable={false}
-          fluid={sources}
-          imgStyle={{
+          style={{
             height: "100%",
-            left: "auto",
-            right: "0",
-            width: "auto",
           }}
-          ref={leftRef}
-          fadeIn={false}
         />
       </div>
-      <div className={parallaxedRubContainer}>
-        <Img
+      <div className={parallaxedRubContainer} ref={rightRef}>
+        <GatsbyImage
+          image={imagesWithArtDirection}
           className={parallaxedRubRight}
-          alt="Alessio Rapini's picture with parallax effect"
+          alt="Alessio Rapini animated"
+          loading="eager"
+          objectFit="contain"
+          objectPosition="right"
           draggable={false}
-          fluid={sources}
-          imgStyle={{
+          style={{
             height: "100%",
-            left: "auto",
-            right: "0",
-            width: "auto",
           }}
-          ref={rightRef}
-          fadeIn={false}
         />
       </div>
     </div>
