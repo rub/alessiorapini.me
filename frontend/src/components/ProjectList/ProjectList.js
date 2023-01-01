29e487232356fbcce7c8c3a76baeb4bcf4a53f40
Alessio Rapini <alessio.rapini@yahoo.it>
2023-01-01 19:28:56 +0100

    Fix ProjectList warnings

--- frontend/src/components/ProjectList/ProjectList.js
+++ frontend/src/components/ProjectList/ProjectList.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useRef, useState } from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import ProjectItem from "../ProjectItem/ProjectItem"
+import React, { useEffect, useRef, useState } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import ProjectItem from '../ProjectItem/ProjectItem';
 import {
   wrapper,
   listWrapper,
   list,
   projectCounterDefault,
   headingDefault,
-} from "./ProjectList.module.css"
+} from './ProjectList.module.css';
 
-export default ProjectList = () => {
+function ProjectList() {
   const projectsQuery = useStaticQuery(graphql`
     query {
       allMarkdownRemark {
@@ -27,133 +27,141 @@ export default ProjectList = () => {
         }
       }
     }
-  `)
+  `);
 
-  const menuItems = useRef(null)
-  const itemsWrapper = useRef(null)
-  const titleItem = useRef([])
+  const menuItems = useRef(null);
+  const itemsWrapper = useRef(null);
+  const titleItem = useRef([]);
 
   // Move the project items to a state
   const [renderItems, setRenderItems] = useState(
     projectsQuery.allMarkdownRemark.nodes
-  )
+  );
 
-  let isScrolling
-  // Disable the image animation on scroll to prevent messing up the UI and performance bottlenecks
-  const disableAnimationonScroll = (e) => {
+  let isScrolling;
+  // Disable the image animation on scroll to prevent messing up the UI and
+  // performance bottlenecks
+  const disableAnimationonScroll = () => {
     if (itemsWrapper.current && titleItem.current) {
-      window.clearTimeout(isScrolling)
-      itemsWrapper.current.style.pointerEvents = "none"
-      renderItems.map(
-        (_, i) => (titleItem.current[i].style.pointerEvents = "none")
-      )
+      window.clearTimeout(isScrolling);
+      itemsWrapper.current.style.pointerEvents = 'none';
+      renderItems.forEach((_, i) => {
+        titleItem.current[i].style.pointerEvents = 'none';
+      });
 
       isScrolling = setTimeout(() => {
-        itemsWrapper.current.style.pointerEvents = "all"
-        renderItems.map(
-          (_, i) => (titleItem.current[i].style.pointerEvents = "all")
-        )
-      }, 300)
+        itemsWrapper.current.style.pointerEvents = 'all';
+        renderItems.forEach((_, i) => {
+          titleItem.current[i].style.pointerEvents = 'all';
+        });
+      }, 300);
     }
-  }
+  };
 
   useEffect(() => {
-    menuItems.current.addEventListener("scroll", disableAnimationonScroll)
+    menuItems.current.addEventListener('scroll', disableAnimationonScroll);
 
     return () => {
-      menuItems.current.removeEventListener("scroll", disableAnimationonScroll)
-    }
-  }, [])
+      menuItems.current.removeEventListener('scroll', disableAnimationonScroll);
+    };
+  }, []);
 
   const cloneItems = () => {
     // Get the height of the first item
     // const itemHeight = menuItems.current.childNodes[0].offsetHeight
-    const itemHeight = itemsWrapper.current.childNodes[0].offsetHeight
+    const itemHeight = itemsWrapper.current.childNodes[0].offsetHeight;
     // Calculate how many elements can fit into the viewport
-    const fitMax = Math.ceil(window.innerHeight / itemHeight)
+    const fitMax = Math.ceil(window.innerHeight / itemHeight);
 
     // Duplicate the items
     const clonedItems = [...renderItems]
       .filter((_, index) => index < fitMax)
-      .map((target) => target)
+      .map((target) => target);
 
     // Add the cloned items to the original ones
-    setRenderItems([...renderItems, ...clonedItems])
-    return clonedItems.length * itemHeight
-  }
+    setRenderItems([...renderItems, ...clonedItems]);
+    return clonedItems.length * itemHeight;
+  };
 
   // Get the current scroll position
   const getScrollPosition = () => {
-    if (menuItems.current) {
-      return (
-        (menuItems.current.pageYOffset || menuItems.current.scrollTop) -
-        (menuItems.current.clientTop || 0)
-      )
-    }
-  }
+    return (
+      (menuItems.current.pageYOffset || menuItems.current.scrollTop) -
+      (menuItems.current.clientTop || 0)
+    );
+  };
 
   const setScrollPosition = (position) => {
-    menuItems.current.scrollTop = position
-  }
+    menuItems.current.scrollTop = position;
+  };
 
   const initScroll = () => {
-    const scrollPosition = getScrollPosition()
+    const scrollPosition = getScrollPosition();
     if (scrollPosition <= 0) {
-      setScrollPosition(1)
+      setScrollPosition(1);
     }
-  }
+  };
 
   useEffect(() => {
-    const clonesHeight = cloneItems()
-    initScroll()
+    const clonesHeight = cloneItems();
+    initScroll();
 
     // Create the loop scroll effect
     const scrollUpdate = () => {
-      const scrollPosition = getScrollPosition()
-      // If the first element of the cloned items hits the top we will scroll back to position 0 of the scroll
+      const scrollPosition = getScrollPosition();
+      // If the first element of the cloned items hits the top we will scroll
+      // back to position 0 of the scroll
       if (clonesHeight + scrollPosition >= itemsWrapper.current.scrollHeight) {
-        setScrollPosition(1)
-        // If the first element of the cloned items hits point 0 we will scroll to cloned items
+        setScrollPosition(1);
+        // If the first element of the cloned items hits point 0 we will scroll
+        // to cloned items
       } else if (scrollPosition <= 0) {
-        setScrollPosition(itemsWrapper.current.scrollHeight - clonesHeight)
+        setScrollPosition(itemsWrapper.current.scrollHeight - clonesHeight);
       }
-    }
+    };
 
-    menuItems.current.addEventListener("scroll", scrollUpdate)
+    menuItems.current.addEventListener('scroll', scrollUpdate);
 
     return () => {
-      menuItems.current.removeEventListener("scroll", scrollUpdate)
-    }
-  }, [])
+      menuItems.current.removeEventListener('scroll', scrollUpdate);
+    };
+  }, []);
 
-  const originalItemsAmount = projectsQuery.allMarkdownRemark.nodes.length
+  const originalItemsAmount = projectsQuery.allMarkdownRemark.nodes.length;
 
   return (
     <div className={wrapper}>
       <div className={listWrapper} ref={menuItems}>
         <ul className={list} ref={itemsWrapper}>
-          {renderItems.map((project, index) => (
-            <ProjectItem
-              key={index}
-              counter={
-                index < originalItemsAmount
-                  ? index + 1
-                  : index + 1 - originalItemsAmount
-              }
-              title={project.frontmatter.title}
-              url={
-                project.frontmatter.featured_image.childImageSharp
-                  .gatsbyImageData
-              }
-              alt={project.frontmatter.title}
-              roles={project.frontmatter.roles}
-              titleRef={(ref) => (titleItem.current[index] = ref)}
-              projectCounterClassName={projectCounterDefault}
-              headingClassName={headingDefault}
-            />
-          ))}
+          {renderItems.map((project, index) => {
+            const { id } = project;
+            return (
+              <ProjectItem
+                key={id}
+                counter={
+                  index < originalItemsAmount
+                    ? index + 1
+                    : index + 1 - originalItemsAmount
+                }
+                title={project.frontmatter.title}
+                url={
+                  project.frontmatter.featured_image.childImageSharp
+                    .gatsbyImageData
+                }
+                alt={project.frontmatter.title}
+                roles={project.frontmatter.roles}
+                titleRef={(ref) => {
+                  titleItem.current[index] = ref;
+                }}
+                projectCounterClassName={projectCounterDefault}
+                headingClassName={headingDefault}
+              />
+            );
+          })}
         </ul>
       </div>
     </div>
-  )
+  );
 }
+
+export default ProjectList;
