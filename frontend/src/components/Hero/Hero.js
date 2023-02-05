a54355debdb9fff02565594cef8cc54f892b91b4
Alessio Rapini <alessio.rapini@yahoo.it>
2023-02-05 23:04:11 +0100

    Improve responsive styles for larger screens

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -1,113 +1,36 @@
-import React from "react"
+import React from 'react';
 
 import {
   hero,
   glassyHeadingFixed,
-  glassyHeadingAnimated,
-  headingAnimated,
-  headingLetterAnimatedWrapper,
-  headingLetterAnimated,
   description,
   descriptionFadeIn,
   role,
-  roleLetterWrapper,
-  roleLetter,
-} from "./Hero.module.css"
+} from './Hero.module.css';
 
-const Hero = () => (
-  <div className={hero}>
-    <h1 className={glassyHeadingFixed}>
-      <svg aria-hidden="true">
-        <clipPath id="lockup-headline-mask-path">
-          <text dominantBaseline="hanging" x="0" y="0" dy="0">
-            ALESSIO RAPINI
-          </text>
-        </clipPath>
-      </svg>
-    </h1>
-    <h1 className={glassyHeadingAnimated}>
-      <span className={headingAnimated}>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>A</span>
-        </span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>L</span>
-        </span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>E</span>
-        </span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>S</span>
-        </span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>S</span>
-        </span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>I</span>
-        </span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>O </span>
-        </span>
-        <span>&nbsp;</span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>R</span>
-        </span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>A</span>
-        </span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>P</span>
-        </span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>I</span>
-        </span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>N</span>
-        </span>
-        <span className={headingLetterAnimatedWrapper}>
-          <span className={headingLetterAnimated}>I</span>
-        </span>
-      </span>
-    </h1>
-    <div className={description}>
-      <p>
-        <span className={descriptionFadeIn}>I am a&nbsp;</span>
-        <span className={role}>
-          <span className={roleLetterWrapper}>
-            <span className={roleLetter}>U</span>
-          </span>
-          <span className={roleLetterWrapper}>
-            <span className={roleLetter}>X</span>
-          </span>{" "}
-          <span className={roleLetterWrapper}>
-            <span className={roleLetter}>D</span>
-          </span>
-          <span className={roleLetterWrapper}>
-            <span className={roleLetter}>E</span>
-          </span>
-          <span className={roleLetterWrapper}>
-            <span className={roleLetter}>S</span>
-          </span>
-          <span className={roleLetterWrapper}>
-            <span className={roleLetter}>I</span>
-          </span>
-          <span className={roleLetterWrapper}>
-            <span className={roleLetter}>G</span>
-          </span>
-          <span className={roleLetterWrapper}>
-            <span className={roleLetter}>N</span>
-          </span>
-          <span className={roleLetterWrapper}>
-            <span className={roleLetter}>E</span>
-          </span>
-          <span className={roleLetterWrapper}>
-            <span className={roleLetter}>R</span>
-          </span>
-        </span>
+// TODO: Try the new animation on Figma first
+// TODO: Run loading screen only once => https://stackoverflow.com/questions/62456913/show-loading-screen-only-once-per-session
+function Hero() {
+  return (
+    <div className={hero}>
+      <h1 className={glassyHeadingFixed}>
+        <svg aria-hidden="true">
+          <clipPath id="lockup-headline-mask-path">
+            <text dominantBaseline="hanging" x="0" y="0" dy="0">
+              ALESSIO RAPINI
+            </text>
+          </clipPath>
+        </svg>
+      </h1>
+      <p className={description}>
+        <p>
+          <span className={descriptionFadeIn}>I am a&nbsp;</span>
+          <span className={role}>UX DESIGNER</span>
+        </p>
+        <p className={descriptionFadeIn}>(who enjoys coding too)</p>
       </p>
-      <p className={descriptionFadeIn}>(who loves to code too)</p>
     </div>
-  </div>
-)
+  );
+}
 
-export default Hero
+export default Hero;
